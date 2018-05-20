8f9830b4ce73278713889166e233ddd2c9086899
anurag <anurag@legalfab.com>
2018-05-20 21:20:55 +0530

    added new google api key

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,22 +24,3 @@
 //= require turbolinks
 //= require_tree .
 
-
-function myMap() {
-    var myCenter = new google.maps.LatLng(1.2967926,103.7845679);
-    var mapCanvas = document.getElementById("map");
-    var mapOptions = {center: myCenter, zoom: 17};
-    var map = new google.maps.Map(mapCanvas, mapOptions);
-    var marker = new google.maps.Marker({position:myCenter});
-    marker.setMap(map);
-
-    var infowindow = new google.maps.InfoWindow({
-        content: "<span class='text-bold'>LegalFAB Private Limited</span><br>\n" +
-        "LegalFAB Private Limited <br>" +
-        "LaunchPad @ One North <br>" +
-        "71 Ayer Rajah Crescent<br>" +
-        "Level 2, Unit 15<br>" +
-        "Singapore 139951 "
-    });
-    infowindow.open(map,marker);
-}
