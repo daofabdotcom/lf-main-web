ef76c7f6c7a717e932f991f4c3a737c0e1d9d948
Suchitra <suchitra@legalfab.com>
2019-02-08 13:35:02 +0530

    adding the latest news

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,14 @@ class ApplicationController < ActionController::Base
     @action = action_name
     params["trans_key"] = ""
     params["trans_key"] = session[:session_id] if session[:session_id].present?
+    @news = []
+    @posts = ButterCMS::Post.all({:page => 1})
+    if @posts.to_a.length > 0
+    @news = @posts.to_a[0..1]
+    end
+    rescue Exception => ex
+      Rails.logger.error "Error occurred in fetching posts from butter cms."
+    end
   end
-end
+
+
